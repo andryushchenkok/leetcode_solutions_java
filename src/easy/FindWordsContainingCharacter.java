7582b8f9b31f67891c9db4191554bb3013e8794e
andryushchenkok <andryushchenko.k@yandex.ru>
2023-11-29 13:42:15 +0300

    2942. Find Words Containing Character

---
+++ src/easy/FindWordsContainingCharacter.java
@@ -0,0 +1,20 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 2942. Find Words Containing Character
+public class FindWordsContainingCharacter {
+
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                answer.add(i);
+            }
+        }
+
+        return answer;
+    }
+}
